ec12c1820bb8bc3696db766d0ce8bdf4712be861
ben-loper <loperb06@gmail.com>
2022-02-28 21:32:51 -0500

    Moved front end code to new sub folder, add initial server code setup

---
+++ Backend/Backend/Controllers/VolunteerController.cs
@@ -0,0 +1,29 @@
+﻿using Backend.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VolunteerController : ControllerBase
+    {
+        private readonly ILogger<VolunteerController> _logger;
+
+        public VolunteerController(ILogger<VolunteerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult SaveVolunteerForm(VolunteerSignUp req)
+        {
+            // Save to DB
+            return Ok();
+        }
+    }
+}
---
+++ Backend/Backend/DTOs/VolunteerSignUp.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.DTOs
+{
+    public class VolunteerSignUp
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public List<string> PreferredContactMethods { get; set; }
+        public List<string> Interests { get; set; }
+        public List<string> Skills { get; set; }
+        public HeadingCommitteeInterestResponse InterestedInHeadingCommittee { get; set; } = HeadingCommitteeInterestResponse.NO;
+    }
+}
